e4fdb1ea808f67dc76d43583ac959a6ba59a3c3a
Belai Beshah <belai.beshah@hexagon.com>
2019-05-03 16:52:21 -0400

    Added timing stats and make default output time.

--- src/ar/com/yamamoto/hartools/HarFile.java
+++ src/ar/com/yamamoto/hartools/HarFile.java
@@ -1,225 +1,253 @@
 package ar.com.yamamoto.hartools;
 
-import org.apache.wink.json4j.JSONArray;
-import org.apache.wink.json4j.JSONException;
-import org.apache.wink.json4j.JSONObject;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.wink.json4j.*;
+import java.io.*;
 import java.nio.charset.UnsupportedCharsetException;
-import java.text.NumberFormat;
-import java.util.Locale;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HarFile extends TextFile {
-    private       JSONObject   json;
-    private       Character    csvDelimiter;
-    private       NumberFormat timingNumberFormat;
-    final private Character    CSV_DEFAULT_DELIMITER = '\t';
-    final private String       timingNames[]         = { "blocked", "dns", "connect", "send", "wait", "receive", "ssl" };
+	private JSONObject json;
+	private Character csvDelimiter;
+	final private Character CSV_DEFAULT_DELIMITER = '\t';
+	final private String timingNames[] = { "wait", "receive", "blocked", "send", "dns", "connect", "ssl" };
+	final private Map<String, DescriptiveStatistics> statistics = new HashMap<String, DescriptiveStatistics>();
+	final private DescriptiveStatistics totalStatistics = new DescriptiveStatistics();
 
-    public HarFile(String filename, String charset, String timingLocaleName)
-        throws FileNotFoundException, IOException, JSONException, UnsupportedCharsetException {
-        super(filename, charset);
-        if (timingLocaleName != null) {
-            final Locale timingLocale = new Locale(timingLocaleName);
-            timingNumberFormat = NumberFormat.getNumberInstance(timingLocale);
-        } else {
-            timingNumberFormat = NumberFormat.getNumberInstance();
-        }
-        init();
-    }
+	public HarFile(String filename, String charset)
+			throws FileNotFoundException, IOException, JSONException, UnsupportedCharsetException {
+		super(filename, charset);
+		init();
+	}
+	
+	public HarFile(String filename)
+			throws FileNotFoundException, IOException, JSONException {
+		super(filename);
+		init();
+	}
+	
+	/**
+	 * Constructor helper
+	 * 
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	private void init()
+			throws FileNotFoundException, IOException, JSONException {
+		readFile();
+		json = new JSONObject(getText());
+		setDelimiter(CSV_DEFAULT_DELIMITER);
+	}
+	
+	/**
+	 * Crawl JSONObject and generate a CSV String for entries
+	 * 
+	 * @return
+	 * 		CSV String
+	 * @throws JSONException
+	 */
+	public String entriesToCsv()
+			throws JSONException {
+		StringBuilder csvBuffer = new StringBuilder();
 
-    public HarFile(String filename, String charset)
-        throws FileNotFoundException, IOException, JSONException, UnsupportedCharsetException {
-        super(filename, charset);
-        init();
-    }
+		// CSV headers
+		csvBuffer.append("url" + csvDelimiter
+			+ "method" + csvDelimiter
+			+ "startedDateTime" + csvDelimiter
+			+ "time" + csvDelimiter
+			+ "Response status" + csvDelimiter
+			+ "Response content mimeType" + csvDelimiter
+			+ "Response content size" + csvDelimiter
+			+ "Response headersSize" + csvDelimiter
+			+ "Response bodySize" + csvDelimiter
+			+ "Referer" + csvDelimiter);
+		
+		for(int i = 0; i < timingNames.length; i++) {
+			csvBuffer.append("Timing " + timingNames[i]);
+			if(i + 1 < timingNames.length) {
+				csvBuffer.append(csvDelimiter);
+			}
+		}
+		
+		csvBuffer.append(LINE_ENDING);
+		
+		JSONArray entries = ((JSONObject)json.get("log")).getJSONArray("entries");
+		
+		for(int i = 0; i < entries.length(); i++) {
+			csvBuffer.append(singleEntryToCsvLine(entries.getJSONObject(i)));
+			if(i < csvBuffer.length() - 1) {
+				csvBuffer.append(LINE_ENDING);
+			}
+		}
 
-    public HarFile(String filename)
-        throws FileNotFoundException, IOException, JSONException {
-        super(filename);
-        init();
-    }
+		System.out.println("Statistics -> Total Timing : #" +  totalStatistics.getN() +
+				"\n Min   = " + totalStatistics.getMin() +
+				"\n Mean   = " + totalStatistics.getMean() +
+				"\n STD    = " + totalStatistics.getStandardDeviation() +
+				"\n Median = " + totalStatistics.getPercentile(50) +
+				"\n 90%    = " + totalStatistics.getPercentile(90) +
+				"\n 99%    = " + totalStatistics.getPercentile(99)  +
+				"\n Max   = " + totalStatistics.getMax()
+		);
 
-    /**
-     * Constructor helper
-     *
-     * @throws FileNotFoundException
-     * @throws IOException
-     * @throws JSONException
-     */
-    private void init()
-        throws FileNotFoundException, IOException, JSONException {
-        readFile();
-        json = new JSONObject(getText());
-        setDelimiter(CSV_DEFAULT_DELIMITER);
-    }
 
-    /**
-     * Crawl JSONObject and generate a CSV String for entries
-     *
-     * @return CSV String
-     * @throws JSONException
-     */
-    public String entriesToCsv()
-        throws JSONException {
-        StringBuilder csvBuffer = new StringBuilder();
 
-        // CSV headers
-        csvBuffer.append("url" + csvDelimiter
-            + "host" + csvDelimiter
-            + "method" + csvDelimiter
-            + "startedDateTime" + csvDelimiter
-            + "time" + csvDelimiter
-            + "Response status" + csvDelimiter
-            + "Response content mimeType" + csvDelimiter
-            + "Response content size" + csvDelimiter
-            + "Response headersSize" + csvDelimiter
-            + "Response bodySize" + csvDelimiter
-            + "Referer" + csvDelimiter);
+		for (String timingName : timingNames) {
 
-        for (int i = 0; i < timingNames.length; i++) {
-            csvBuffer.append("Timing " + timingNames[i]);
-            if (i + 1 < timingNames.length) {
-                csvBuffer.append(csvDelimiter);
-            }
-        }
+			DescriptiveStatistics descriptiveStatistics = statistics.get(timingName);
 
-        csvBuffer.append(LINE_ENDING);
 
-        JSONArray entries = ((JSONObject) json.get("log")).getJSONArray("entries");
+			System.out.println("Statistics -> " + timingName + " for : #" +  descriptiveStatistics.getN() +
+					"\n Min   = " + descriptiveStatistics.getMin() +
+					"\n Mean   = " + descriptiveStatistics.getMean() +
+					"\n STD    = " + descriptiveStatistics.getStandardDeviation() +
+					"\n Median = " + descriptiveStatistics.getPercentile(50) +
+					"\n 90%    = " + descriptiveStatistics.getPercentile(90) +
+					"\n 99%    = " + descriptiveStatistics.getPercentile(99)  +
+					"\n Max   = " + descriptiveStatistics.getMax()
+			);
+		}
 
-        for (int i = 0; i < entries.length(); i++) {
-            csvBuffer.append(singleEntryToCsvLine(entries.getJSONObject(i)));
-            if (i < csvBuffer.length() - 1) {
-                csvBuffer.append(LINE_ENDING);
-            }
-        }
 
-        return csvBuffer.toString();
-    }
+		return csvBuffer.toString();
+	}
+	
+	/**
+	 * Process a single entry
+	 * 
+	 * @param jsonObject
+	 * @return
+	 * 		CSV String line
+	 * @throws JSONException
+	 */
+	private String singleEntryToCsvLine(JSONObject jsonObject)
+			throws JSONException {
+		StringBuilder csvLineBuffer = new StringBuilder();
+		
+		JSONObject requestObject = jsonObject.getJSONObject("request");
+		JSONObject responseObject = jsonObject.getJSONObject("response");
+		JSONObject contentObject = responseObject.getJSONObject("content");
+		JSONObject timingsObject = jsonObject.getJSONObject("timings");
 
-    /**
-     * Process a single entry
-     *
-     * @param jsonObject
-     * @return CSV String line
-     * @throws JSONException
-     */
-    private String singleEntryToCsvLine(JSONObject jsonObject)
-        throws JSONException {
-        StringBuilder csvLineBuffer = new StringBuilder();
+		// General keys
+		csvLineBuffer.append(processKey(requestObject, "url") + csvDelimiter);
+		csvLineBuffer.append(processKey(requestObject, "method") + csvDelimiter);
+		csvLineBuffer.append(processKey(jsonObject, "startedDateTime") + csvDelimiter);
 
-        JSONObject requestObject = jsonObject.getJSONObject("request");
-        JSONObject responseObject = jsonObject.getJSONObject("response");
-        JSONObject contentObject = responseObject.getJSONObject("content");
-        JSONObject timingsObject = jsonObject.getJSONObject("timings");
+		csvLineBuffer.append(processKey(responseObject, "status") + csvDelimiter);
+		csvLineBuffer.append(processKey(contentObject, "mimeType") + csvDelimiter);
+		csvLineBuffer.append(processKey(contentObject, "size") + csvDelimiter);
+		csvLineBuffer.append(processKey(responseObject, "headersSize") + csvDelimiter);
+		csvLineBuffer.append(processKey(responseObject, "bodySize") + csvDelimiter);
+		csvLineBuffer.append(processKey(
+				requestObject.getJSONArray("headers"), "Referer") + csvDelimiter);
 
-        final String url = processKey(requestObject, "url");
 
-        // General keys
-        csvLineBuffer.append(url + csvDelimiter);
-        csvLineBuffer.append(hostNameFromUrl(url) + csvDelimiter);
-        csvLineBuffer.append(processKey(requestObject, "method") + csvDelimiter);
-        csvLineBuffer.append(processKey(jsonObject, "startedDateTime") + csvDelimiter);
-        csvLineBuffer.append(processKey(jsonObject, "time") + csvDelimiter);
-        csvLineBuffer.append(processKey(responseObject, "status") + csvDelimiter);
-        csvLineBuffer.append(processKey(contentObject, "mimeType") + csvDelimiter);
-        csvLineBuffer.append(processKey(contentObject, "size") + csvDelimiter);
-        csvLineBuffer.append(processKey(responseObject, "headersSize") + csvDelimiter);
-        csvLineBuffer.append(processKey(responseObject, "bodySize") + csvDelimiter);
-        csvLineBuffer.append(processKey(
-            requestObject.getJSONArray("headers"), "Referer") + csvDelimiter);
+		//save stats for total time
+		String timeValue = processKey(jsonObject, "time");
+		if(null != timeValue && !timeValue.isEmpty()) {
+			totalStatistics.addValue(Double.valueOf(timeValue));
+		}
+		csvLineBuffer.append(timeValue + csvDelimiter);
 
-        // Timing keys
-        for (int i = 0; i < timingNames.length; i++) {
-            csvLineBuffer.append(processKey(timingsObject, timingNames[i]));
-            if (i + 1 < timingNames.length) {
-                csvLineBuffer.append(csvDelimiter);
-            }
-        }
+		// Timing keys
+		for(int i = 0; i < timingNames.length; i++) {
+			String value = processKey(timingsObject, timingNames[i]);
 
-        return csvLineBuffer.toString();
-    }
+			csvLineBuffer.append(value);
 
-    private String hostNameFromUrl(final String urlString) {
-        if (urlString != null) {
-            final String urlLower = urlString.toLowerCase();
-            if (urlLower.startsWith("http://") || urlLower.startsWith("https://")) {
-                try {
-                    URL url = new URL(urlString);
-                    return url.getHost();
-                } catch (MalformedURLException e) {
-                    // fall through
-                }
-            }
-        }
-        return "";
-    }
+			DescriptiveStatistics descriptiveStatistics = statistics.get(timingNames[i]);
 
-    /**
-     * Process key and return value as String
-     * (empty String, if not applicable or null)
-     *
-     * @param object JSONObject
-     * @param key    Name of the key
-     * @return Value of the key, or an empty String
-     * @throws JSONException
-     */
-    private String processKey(JSONObject object, String key)
-        throws JSONException {
-        String returnValue = new String();
+			if(descriptiveStatistics ==null) {
+				descriptiveStatistics = new DescriptiveStatistics();
+				statistics.put(timingNames[i], descriptiveStatistics);
+			}
 
-        if (object.has(key)) {
-            if (!object.isNull(key)) {
-                Object value = object.get(key);
-                if (String.class.isInstance(value)) {
-                    if (value != null) {
-                        returnValue = (String) value;
-                    }
-                } else if (value instanceof Number) {
-                    if (((Number) value).doubleValue() > 0.0) {
-                        returnValue = timingNumberFormat.format(value);
-                    }
-                }
-            }
-        }
+			if(null != value && !value.isEmpty()) {
+				descriptiveStatistics.addValue(Double.valueOf(value));
+			}
 
-        return returnValue;
-    }
 
-    /**
-     * Process key from JSONArray, and return value as String
-     *
-     * @param jsonArray JSONArray
-     * @param key       Name of the JSONObject whose value we want to retrieve
-     * @return Value as String, or an empty string if the
-     * key was not found
-     * @throws JSONException
-     */
-    private String processKey(JSONArray jsonArray, String key)
-        throws JSONException {
-        String returnValue = new String();
+			if(i + 1 < timingNames.length) {
+				csvLineBuffer.append(csvDelimiter);
+			}
+		}
+						
+		return csvLineBuffer.toString();
+	}
 
-        for (Object entry : jsonArray.toArray()) {
-            if (((JSONObject) entry).getString("name").equalsIgnoreCase(key)) {
-                returnValue = processKey((JSONObject) entry, "value");
-                break;
-            }
-        }
-
-        return returnValue;
-    }
-
-    /**
-     * Set CSV delimiter
-     *
-     * @param delimiter
-     */
-    public void setDelimiter(Character delimiter) {
-        if (delimiter != null)
-            csvDelimiter = delimiter;
-    }
+	/**
+	 * Process key and return value as String
+	 * (empty String, if not applicable or null)
+	 * 
+	 * @param object
+	 * 		JSONObject
+	 * @param key
+	 * 		Name of the key
+	 * @return
+	 * 		Value of the key, or an empty String
+	 * @throws JSONException
+	 */
+	private String processKey(JSONObject object, String key)
+			throws JSONException {
+		String returnValue = new String();
+		
+		if(object.has(key)) {
+			if(!object.isNull(key)) {
+				Object value = object.get(key);
+				if(String.class.isInstance(value)) {
+					if(value != null) {
+						returnValue = (String)value;
+					}
+				} else if(Integer.class.isInstance(value)) {
+					if((Integer)value >= 0) {
+						returnValue = String.valueOf(value);
+					}
+				} else if(Double.class.isInstance(value)) {
+					if((Double)value >= 0) {
+						returnValue = String.valueOf(value);
+					}
+				}
+			}
+		}
+		
+		return returnValue;
+	}
+	
+	/**
+	 * Process key from JSONArray, and return value as String
+	 * @param jsonArray
+	 * 		JSONArray
+	 * @param key
+	 * 		Name of the JSONObject whose value we want to retrieve
+	 * @return
+	 * 		Value as String, or an empty string if the
+	 * 		key was not found
+	 * @throws JSONException
+	 */
+	private String processKey(JSONArray jsonArray, String key)
+			throws JSONException {
+		String returnValue = new String();
+		
+		for(Object entry : jsonArray.toArray()) {
+			if(((JSONObject)entry).getString("name").equalsIgnoreCase(key)) {
+				returnValue = processKey((JSONObject)entry, "value");
+				break;
+			}
+		}
+		
+		return returnValue;
+	}
+	
+	/**
+	 * Set CSV delimiter
+	 * 
+	 * @param delimiter
+	 */
+	public void setDelimiter(Character delimiter) {
+		if(delimiter != null)
+			csvDelimiter = delimiter;
+	}
 }
--- src/ar/com/yamamoto/hartools/har2csv.java
+++ src/ar/com/yamamoto/hartools/har2csv.java
@@ -16,12 +16,11 @@ public class har2csv {
 		argsTrans.put('e', "encoding");
 		argsTrans.put('h', "help");
 		argsTrans.put('i', "in");
-		argsTrans.put('o', "out");
-		argsTrans.put('t', "timing-locale");
+		argsTrans.put('o', "out");		
 		argsTrans.put('v', "version");
-
+				
 		CliArgs cliArgs = new CliArgs(args, argsTrans);
-
+		
 		if(cliArgs.hasArg("help")) {
 			System.out.println("Usage: har2csv options\n"
 				+ "where options include:\n"
@@ -33,49 +32,50 @@ public class har2csv {
 				+ "    -i, --in <filename>       Source HAR filename\n"
 				+ "    -o, --out <filename>      Destination CSV filename\n"
 				+ "                              (if omitted, outputs to stdout)\n"
-				+ "    -t, --timing-locale <l>   Locale to use for the timing output.\n"
 				+ "    -v, --version             Version number\n");
 		} else if(cliArgs.hasArg("version")) {
-			System.out.println("har2csv $build:20180828_1434$\n"
+			System.out.println("har2csv $build:20190503_1646$\n"
 				+ "HTTP Archive(har) to CSV converter\n"
 				+ "http://www.yamamoto.com.ar/hartools\n");
 		} else if(cliArgs.hasArg("in") && String.class.isInstance(cliArgs.getValue("in"))) {
 			HarFile harFile;
-
-			// Get encoding from arguments, if exists
-			String inputEncoding = (String) cliArgs.getValue("encoding");
-			if (inputEncoding == null) {
-				inputEncoding = "UTF-8";
-			}
-
-			// Get output timing locale
-			String timingLocale = (String) cliArgs.getValue("timing-locale");
-
+			
 			try {
-				// Call the appropriate constructor
-				harFile = new HarFile((String)cliArgs.getValue("in"), inputEncoding, timingLocale);
-
+				// Get encoding from arguments, if exists, and call the appropiate constructor				
+				if(cliArgs.hasArg("encoding") && String.class.isInstance(cliArgs.getValue("encoding"))) {
+					harFile = new HarFile((String)cliArgs.getValue("in"), (String)cliArgs.getValue("encoding"));
+				} else {
+					harFile = new HarFile((String)cliArgs.getValue("in"));
+				}
+				
 				// Get delimiter from arguments, if exists
 				if(cliArgs.hasArg("delimiter") && String.class.isInstance(cliArgs.getValue("delimiter"))) {
 					harFile.setDelimiter(((String)cliArgs.getValue("delimiter")).charAt(0));
 				}
-
+				
 				// The actual conversion
 				String csvString = harFile.entriesToCsv();
-
+				
 				// Get destination filename and save content, or print to stdout
 				if(cliArgs.hasArg("out") && String.class.isInstance(cliArgs.getValue("out"))) {
 					TextFile csvFile = new TextFile((String)cliArgs.getValue("out"), "utf-8", true);
 					csvFile.writeToFile(csvString);
 				} else {
-					System.out.print(harFile.entriesToCsv());
+					String filename = (String)cliArgs.getValue("in");
+					if (filename.contains(".")) {
+						filename = filename.substring(0, filename.lastIndexOf('.'));
+					}
+					filename += "." + "csv";
+					TextFile csvFile = new TextFile(filename, "utf-8", true);
+
+					csvFile.writeToFile(csvString);
 				}
 			} catch(FileNotFoundException Ex) {
 				System.out.println("[ERROR] File not found (\""
 					+ (String)cliArgs.getValue("in") + "\")\n");
 			} catch(UnsupportedCharsetException Ex) {
 				System.out.println("[ERROR] Unknown encoding (\""
-					+ (String) inputEncoding + "\")\n");
+					+ (String)cliArgs.getValue("encoding") + "\")\n");
 			} catch(IOException Ex) {
 				System.out.println("[ERROR] " + Ex.toString() + "\n");
 			} catch(JSONException Ex) {
